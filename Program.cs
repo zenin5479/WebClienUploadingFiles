f9ddf158962d4322c1b0c086f18f2c6dc2a4cbae
zenin5479 <zenin5479@gmail.com>
2025-11-27 13:25:13 +0300

    Update 0.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace WebClienUploadingFiles
 {
@@ -6,7 +8,28 @@ namespace WebClienUploadingFiles
    {
       static void Main()
       {
-         Console.WriteLine("Hello World!");
+         Console.WriteLine("Загрузка...Пожалуйста, подождите...");
+         // Для загрузки файла используется класс WebClient.
+         var wc = new WebClient();
+         // Метод DownloadFile() принимает два параметра - первый это путь к файлу,
+         // который нужно скачать, а второй - локальное имя файла.
+         wc.DownloadFile("https://www.google.ru", "UploadingFilesWebClient.txt");
+         Console.WriteLine("Загрузка завершена");
+         // Допустим, нужно получить какую - то веб - страницу в строку для последующей ее обработки.
+         // Cама процедура неэффективная - сначала мы загружаем страницу в файл, потом читаем текст из этого файла.
+         // Гораздо удобнее использовать метод OpenRead().
+         var str = wc.OpenRead("https://www.google.ru");
+         // Содержимое страницы будет загружено в переменную.
+         // После этого можно использовать класс StreamReader для обработки потока.
+         if (str != null)
+         {
+            var sr = new StreamReader(str);
+            string s;
+            while ((s = sr.ReadLine()) != null)
+               Console.WriteLine(s);
+         }
+
+         Console.ReadLine();
       }
    }
 }
